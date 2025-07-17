86561ead8bff78220aee154310e8ceb7bbda1b2d
Kurama <zozulya.workmail@gmail.com>
2025-07-17 13:42:45 +0300

    Created: Classes: BaseRobot, FlyingRobot, DeliveryDrone

--- app/main.py
+++ app/main.py
@@ -48,12 +48,12 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  coords: list = (0, 0, 0),
-                 current_load: int = None) -> None:
+                 current_load: Cargo = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
-    def hook_load(self, cargo: object) -> None:
+    def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
